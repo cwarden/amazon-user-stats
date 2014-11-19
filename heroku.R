4ebf5e003a22445cc26e02e1f00ee0e1e6bd0941
Christian G. Warden <cwarden@xerus.org>
2014-11-18 20:26:22 -0800

    Load .env only if it exists

--- heroku.R
+++ heroku.R
@@ -1,5 +1,5 @@
+library(dotenv)
 src_heroku <- function() {
-	dotenv::load_dot_env()
 	db_url <- Sys.getenv('DATABASE_URL')
 	db <- httr::parse_url(db_url)
 	dplyr::src_postgres(user = db$username, password = db$password, host = db$hostname, port = db$port, dbname = db$path)
